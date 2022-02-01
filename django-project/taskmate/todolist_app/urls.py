b43da6204e8ded03f40ab73a7a7301c9e7ba567e
7thmaniac <varanasidbzt@gmail.com>
2022-02-02 05:17:51 +0530

    intermediate changes

--- django-project/taskmate/todolist_app/urls.py
+++ django-project/taskmate/todolist_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from todolist_app import views
 
 urlpatterns = [
-    path('', views.todolist, name = 'todolist'),
+    path('todolist', views.todolist, name = 'todolist'),
+    path('contact_us', views.contact, name = 'contact_us'),
+    path('about_us', views.about, name = 'about_us'),
 
 ]
--- django-project/taskmate/todolist_app/views.py
+++ django-project/taskmate/todolist_app/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def todolist(request):
-    return render(request, 'todolist.html', {})
+    context = {'todolist_text':'Welcome to Todo List page'}
+    return render(request, 'todolist.html', context )
+
+def contact(request):
+    context = {'contact_text':'Welcome to contact page'}
+    return render(request, 'contact_us.html', context )
+
+def about(request):
+    context = {'about_text':'Welcome to about page'}
+    return render(request, 'about_us.html', context )
