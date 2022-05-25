20a9b4ac0a3535190c1c224aad0e173dc3401283
7thmaniac <varanasidbzt@gmail.com>
2022-05-25 17:23:02 +0530

    created heroku app named as tm7app

--- django-project/taskmate/taskmate/settings.py
+++ django-project/taskmate/taskmate/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os.path
 import environ
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'todolist'
 
 LOGIN_URL = 'login'
+
+django_heroku.settings(locals())
