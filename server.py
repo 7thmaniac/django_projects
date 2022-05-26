f7f6e4878c42d0d45ce8766fb60d898cc005b71a
7thmaniac <varanasidbzt@gmail.com>
2022-05-26 20:56:47 +0530

    modified server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from waitress import serve
 
-from taskmate.wsgi import application
+from taskmate.taskmate.wsgi import application
 
 if __name__ == '__main__':
-    serve(application, port='8000')
+    serve(application)
